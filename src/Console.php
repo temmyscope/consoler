a15d4fc2b516fb80047e51bed1fa493b5ccc981f
temmyscope <esdentp@gmail.com>
2020-10-26 07:40:14 +0100

    Necessary updates

--- src/Console.php
+++ src/Console.php
@@ -34,7 +34,7 @@ class Console
         $name = $argv[0] ?? self::receiveInput('controller');
         $name = strtolower(str_ireplace('controller', '', $name));
         $nm = ucfirst($name);
-        self::writeToFile(ROOT . DS . "app" . DS . "Controllers", "{$nm}Controller.php", "<?php \nnamespace App\Controllers;\n\nuse App\Providers\Strings;\n\nclass {$nm}Controller extends Controller{\n\n\tpublic function IndexEndPoint(){ \n\t\tview('{$name}.index'); \n\t}\n}");
+        self::writeToFile(ROOT . DS . "app" . DS . "Controllers", "{$nm}Controller.php", "<?php \nnamespace App\Controllers;\n\nuse Seven\Vars\Strings;\n\nclass {$nm}Controller extends Controller{\n\n\tpublic function index(){ \n\t\tview('{$name}.index'); \n\t}\n}");
         self::generateModel($name);
         print("{$nm}Controller and corresponding Model has been generated.\n\n");
     }
@@ -54,19 +54,29 @@ class Console
         $table = strtolower($name);
         $nm = str_replace(" ", "", ucwords(str_replace('_', " ", $name)));
         $fileName = $nm . ".php";
-        self::writeToFile(ROOT . DS . "app", $fileName, "<?php \nnamespace App;\n\nuse App\Providers\Model; \n\nclass {$nm} extends Model{\n\n\tprotected static \$table = '{$table}'; \n\n\tprotected static \$fulltext = []; \n\n\tprotected static \$fetchable = [];\n\n}");
+        self::writeToFile(ROOT . DS . "app", $fileName, "<?php \nnamespace App;\n\nuse Seven\Model\Model; \n\nclass {$nm} extends Model{\n\n\tprotected static \$table = '{$table}'; \n\n\tprotected static \$fulltext = []; \n\n\tprotected static \$fetchable = [];\n\n}");
     }
 
     public static function db($argv)
     {
         $db = $argv[0] ?? self::receiveInput();
-        return Schema::db(strtolower($db));
+        if ((new SchemaMap)->db(strtolower($db))){
+            self::changeLine(ROOT.DS.'.env', "DB_NAME", "DB_NAME={$db}\n");
+            echo "\n.env File has been updated";
+        }
+    }
+
+    public static function changeLine($file, $line, $replacement)
+    {
+        file_put_contents($file, implode('', array_map(function ($data) use ($line, $replacement) {
+            return (strstr($data, $line)) ? $replacement : $data;
+        }, file($file))));
     }
 
     public static function migrate($argv)
     {
         $schema = new SchemaMap([
-            'directory' => __DIR__.DS.'migration',
+            'directory' => ROOT . DS . 'migration',
             'migrator'  => 'Migration.php',
             'populator' => 'Population.php'
         ]);
@@ -76,7 +86,7 @@ class Console
     public function populate()
     {
         $schema = new SchemaMap([
-            'directory' => __DIR__.DS.'migration',
+            'directory' => ROOT . DS . 'migration',
             'migrator'  => 'Migration.php',
             'populator' => 'Population.php'
         ]);
@@ -87,29 +97,47 @@ class Console
     {
         echo "Do you have a development backup for your project, (Y/N)?\n";
         $fp = fopen('php://stdin', 'r');
-        $data = strtolower( fgets($fp, 1024) );
+        $data = strtolower(fgets($fp, 1024));
         if ($data === 'y') {
             return self::removeDevTools();
-        }elseif ($data === 'n') {
+        } elseif ($data === 'n') {
             echo "Please create a development backup, then re-run this command.";
             return;
-        }else{
-            exit("Invalid input.")
+        } else {
+            exit("Invalid input.");
+        }
+    }
+
+    public static function directoryRemover(string $dir)
+    {
+        if(is_dir($dir)){
+            $it = new \RecursiveDirectoryIterator($dir, \RecursiveDirectoryIterator::SKIP_DOTS);
+            $files = new \RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
+            foreach($files as $file) {
+                if ($file->isDir()){
+                    rmdir($file->getRealPath());
+                } else {
+                    unlink($file->getRealPath());
+                }
+            }
+            rmdir($dir);
         }
     }
 
     public static function removeDevTools()
     {
         shell_exec("composer remove phpunit/phpunit");
-        rmdir(ROOT.DS.'migration'.DS);
-        rmdir(ROOT.DS.'tests'.DS);
-        rmdir(ROOT.DS.'Engineer');
-        rmdir(ROOT.DS.'.gitignore');
-        rmdir(ROOT.DS.'Migration.History');
-        rmdir(ROOT.DS.'readme.md');
+        self::directoryRemover(ROOT . DS . 'migration');
+        self::directoryRemover(ROOT . DS . 'tests');
+        self::directoryRemover(ROOT . DS . '.git');
+        unlink(ROOT . DS . 'composer.json');
+        unlink(ROOT . DS . 'composer.lock');
+        unlink(ROOT . DS . 'Engineer');
+        unlink(ROOT . DS . '.gitignore');
+        unlink(ROOT . DS . 'Migration.History');
+        unlink(ROOT . DS . 'readme.md');
         shell_exec("composer remove seven/consoler");
-        shell_exec("composer -o");
-        file_put_contents(ROOT.DS."index.php", implode('', array_map(function ($data) {
+        file_put_contents(ROOT . DS . "index.php", implode('', array_map(function ($data) {
             return (strstr($data, "//\$router->enableCache(__DIR__.'/cache');")) ?
                 "\$router->enableCache(__DIR__.'/cache');" : $data;
         }, file($file))));
@@ -121,7 +149,7 @@ class Console
             mkdir($directory);
         }
         if (file_exists($directory . DS . $fileName)) {
-            echo"File already exists";
+            echo"File already exists\n";
             return;
         }
         $fh = fopen($directory . DS . $fileName, 'w+');
@@ -135,10 +163,7 @@ class Console
         echo "Enter your {$var} name:\n";
         $fp = fopen('php://stdin', 'r');
         $data = fgets($fp, 1024);
-        if (!ctype_alnum($data)) {
-            echo "Only Alpha-numeric characters are allowed.";
-            exit;
-        }
+        $data = trim($data);
         return $data;
     }
 
@@ -155,56 +180,52 @@ class Console
     public static function configureApp($name)
     {
         $vw = "<?php \nreturn [
-	#App settings
-	'APP_NAME' => '{$name}',
-	'APP_DEBUG' => true,
-	'APP_URL' => 'http://localhost/{$name}',
-	'APP_CDN' => 'http://localhost/{$name}/cdn',
-	'APP_ROOT' => __DIR__.'/..',
-	'APP_PUSH_ICON' => '', //file address to app push notification icon
+    #App settings
+    'APP_NAME' => '{$name}',
+    'APP_DEBUG' => true,
+    'APP_URL' => 'http://localhost/{$name}',
+    'APP_CDN' => 'http://localhost/{$name}/cdn',
+    'APP_ROOT' => __DIR__.'/..',
+    'APP_PUSH_ICON' => '', //file address to app push notification icon
 
-	#Mail
-	'APP_EMAIL' => '',
-	
-	#Sessions & Cookies
-	'CURRENT_USER_SESSION_NAME' => '',
-	'REMEMBER_ME_COOKIE_NAME' => '',
-	'REMEMBER_ME_COOKIE_EXPIRY' => 2592000,
-	'REDIRECT' => '',
-
-
-	#Files, Filesystem and Storage Upload Settings
-	'cdn' => __DIR__.'/../public/cdn',
-	'view' => __DIR__.'/../public/view',
-	'assets' => 'http://localhost/{$name}/public/assets',
-	'cache' => __DIR__.'/../cache',
-	'upload_limit' => 5024768,
-	'allowed_files' => [ 
-		'jpg' => 'image/jpeg',
-		'png' => 'image/png', 
-		'jpeg' => 'image/jpeg'
-	],
+    #Mail
+    'APP_EMAIL' => '',
+    
+    #Sessions & Cookies
+    'CURRENT_USER_SESSION_NAME' => '',
+    'REMEMBER_ME_COOKIE_NAME' => '',
+    'REMEMBER_ME_COOKIE_EXPIRY' => 2592000,
+    'REDIRECT' => '',
 
 
-	#Database Migration Settings
-	'ENGINE' => [
-		'TYPE' => 'SQL',
-		'CHARSET' => 'utf8mb4',
-		'COLLATE' => 'utf8mb4_unicode_ci',
-		'GENERATE_MODEL' => true,
-		'MIGRATIONS' => [ 'users', 'user_sessions', 'contact_us' ],
-	],
+    #Files, Filesystem and Storage Upload Settings
+    'cdn' => __DIR__.'/../public/cdn',
+    'view' => __DIR__.'/../public/view',
+    'assets' => 'http://localhost/{$name}/public/assets',
+    'cache' => __DIR__.'/../cache',
+    'upload_limit' => 5024768,
+    'allowed_files' => [ 
+        'jpg' => 'image/jpeg',
+        'png' => 'image/png', 
+        'jpeg' => 'image/jpeg'
+    ],
 
-	#Html Templates
-	/*----------------------------------------------------------------------------------------------|
-	|								ALTVEL/LARAFELL NAVIGATION BAR									|
-	|-----------------------------------------------------------------------------------------------|
-	|	this helps in setting the menu bar for guest users and loggged in users based on the array 	|
-	|	associative arrays can be used for menus with dropdown... 								    | 
-	-----------------------------------------------------------------------------------------------*/
 
-	'USER_NAVBAR' => ['Home' => 'home', 'Search' => 'search', 'Logout' => 'logout'],
-	'GUEST_NAVBAR' => ['Login' => 'login', 'Register' => 'register', 'About' => 'about'],
+    #Database Migration Settings
+    'ENGINE' => [
+        'GENERATE_MODEL' => true
+    ],
+
+    #Html Templates
+    /*----------------------------------------------------------------------------------------------|
+    |                               ALTVEL/LARAFELL NAVIGATION BAR                                  |
+    |-----------------------------------------------------------------------------------------------|
+    |   this helps in setting the menu bar for guest users and loggged in users based on the array  |
+    |   associative arrays can be used for menus with dropdown...                                   | 
+    -----------------------------------------------------------------------------------------------*/
+
+    'USER_NAVBAR' => ['Home' => 'home', 'Search' => 'search', 'Logout' => 'logout'],
+    'GUEST_NAVBAR' => ['Login' => 'login', 'Register' => 'register', 'About' => 'about'],
 
 ];";
         self::writeToFile(ROOT . DS . 'config', 'app.php', $vw);
@@ -214,23 +235,42 @@ class Console
         self::configureREDIRECT($config);
         self::configureCOOKIE($config);
         self::configureSESSION($config);
-        self::configureSALT(ROOT . DS . '.env');
+        self::configureEnvironment($name);
         print("Environment Security Configurations have been successfully set up.\n");
         print("Please rename your root folder (i.e. your current folder) to {$name}. \n");
-        print("
-            You may still have to manually setup some configurations for a production\n
-            server in the config/app.php file of your application.\n
-        ");
+        print("See config/app.php & .env files of your application to make changes.\n");
         exit();
     }
 
-    public function configureEnvironment()
+    public function configureEnvironment($name)
     {
         self::writeToFile(
-            ROOT, 
-            '.env', 
-            "APP_NAME=\nAPP_DEBUG=false\nAPP_ALG=\n APP_SALT=\nAPP_IV=\nAPP_URL=http://localhost\n
-            DB_HOST=localhost\nDB_NAME=altvel\nDB_USER=root\nDB_PASS=\nDB_DRIVER=mysql");
+            ROOT,
+            '.env',
+            "APP_NAME={$name}\nAPP_TIMEZONE=UTC\nAPP_DEBUG=false\nAPP_ALG=\nAPP_SALT=\nAPP_IV=\nAPP_URL=http://localhost/{$name}\nDB_HOST=localhost\nDB_NAME=\nDB_USER=root\nDB_PASS=\nDB_DRIVER=pdo_mysql\nCOLLATE='utf8mb4_unicode_ci'\n"
+        );
+        $file = ROOT . DS . '.env';
+        $ciphers = openssl_get_cipher_methods();
+        $vulnerables = [ "ecb", "des", "rc2", "rc4", "md5"];
+        foreach ($vulnerables as $key => $value) {
+            $ciphers = array_filter($ciphers, function ($n) use ($value) {
+                return stripos($n, $value) === false;
+            });
+        }
+        $ciphers = array_values($ciphers);
+        $limit = count($ciphers) - 1;
+        $cipher = $ciphers[ random_int(0, $limit)];
+        file_put_contents($file, implode('', array_map(function ($data) use ($cipher) {
+            $iv = base64_encode(openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipher)));
+            return (strstr($data, "APP_IV")) ? "APP_IV='{$iv}'\n" : $data;
+        }, file($file))));
+        file_put_contents($file, implode('', array_map(function ($data) use ($cipher) {
+            return (strstr($data, "APP_ALG")) ? "APP_ALG='{$cipher}'\n" : $data;
+        }, file($file))));
+        file_put_contents($file, implode('', array_map(function ($data) {
+            $const = Strings::Rand(32);
+            return (strstr($data, "APP_SALT")) ? "APP_SALT='{$const}'\n" : $data;
+        }, file($file))));
     }
 
     public static function configureTitle($file, $title)
@@ -281,34 +321,6 @@ class Console
 
             $const = Strings::Rand(32);
             return (strstr($data, "'CURRENT_USER_SESSION_NAME'")) ? "\t'CURRENT_USER_SESSION_NAME' => '{$const}',\n" : $data;
-        }, file($file))));
-    }
-
-    public static function configureSALT($file)
-    {
-        $ciphers = openssl_get_cipher_methods();
-        $vulnerables = [ "ecb", "des", "rc2", "rc4", "md5"];
-        foreach ($vulnerables as $key => $value) {
-            $ciphers = array_filter($ciphers, function ($n) {
-                 return stripos($n, $value) === false;
-            });
-        }
-        $ciphers = array_values($ciphers);
-        $limit = count($ciphers) - 1;
-        $cipher = $ciphers[ random_int(0, $limit)];
-        file_put_contents($file, implode('', array_map(function ($data) use ($cipher) {
-
-            $iv = base64_encode(openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipher)));
-            return (strstr($data, "APP_IV")) ? "\tAPP_IV='{$iv}'\n" : $data;
-        }, file($file))));
-        file_put_contents($file, implode('', array_map(function ($data) use ($cipher) {
-
-            return (strstr($data, "APP_ALG")) ? "\tAPP_ALG='{$cipher}'\n" : $data;
-        }, file($file))));
-        file_put_contents($file, implode('', array_map(function ($data) {
-
-            $const = Strings::Rand(32);
-            return (strstr($data, "APP_SALT")) ? "\tAPP_SALT='{$const}'\n" : $data;
         }, file($file))));
     }
 }
--- src/SchemaMap.php
+++ src/SchemaMap.php
@@ -1,195 +1,199 @@
 <?php
+
 namespace Seven\Consoler;
 
-use \PDO;
-use \PDOException;
-
+use PDO;
+use PDOException;
 use Seven\Model\Model;
 
-class SchemaMap{
+class SchemaMap
+{
 
-	public function __construct($config = [
-		'directory' => __DIR__,
-		'migrator'  => 'Migration.php',
-		'populator' => 'Population.php'
-	]){
-		$this->directory = $config['directory'];
-		$this->populator = $this->directory.DIRECTORY_SEPARATOR.$config['migrator'];
-		$this->migrator = $this->directory.DIRECTORY_SEPARATOR.$config['migrator'];
-		if (empty($config)) {
-			exit("Something is Wrong:\n
-				1. Check that you passed a valid configuration parameter to the __construct.\n
-				2. Ensure that your file is a valid php file returning an array E.g. <?php \nreturn [\n];
-			");
-		}
-		$this->time = (new \DateTime('now', new \DateTimeZone('UTC')))->format('Y-m-d H:i:s');
-	}
-
-	public function db($value)
-	{
-		try {
-			$user = getenv('DB_USER');
-			$password = getenv('DB_PASS');
-			$server = getenv('DB_HOST');
-			$db = str_replace("pdo_", '', getenv('DB_DRIVER'));
-			$collate = getenv("COLLATE", "utf8mb4_unicode_ci");
-    	$conn = new PDO("$db:host=$server;", $user, $password);
-    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    	$conn->exec("CREATE DATABASE {$value} COLLATE $col;");
-    	$this->log("DATABASE: \n\t[ NAME => $value, MODE => CREATE, CREATED_AT => {$time} ]\n");
-    }catch(PDOException $e){
-	    echo $e->getMessage();
+    public function __construct($config = [
+        'directory' => '',
+        'migrator'  => 'Migration.php',
+        'populator' => 'Population.php'
+    ])
+    {
+        $this->directory = $config['directory'];
+        $this->populator = $this->directory . DIRECTORY_SEPARATOR . $config['migrator'];
+        $this->migrator = $this->directory . DIRECTORY_SEPARATOR . $config['migrator'];
+        if (empty($config)) {
+            exit("Something is Wrong:\n
+                1. Check that you passed a valid configuration parameter to the __construct.\n
+                2. Ensure that your file is a valid php file returning an array E.g. <?php \nreturn [\n];
+            ");
+        }
+        $this->time = (new \DateTime('now', new \DateTimeZone(env('APP_TIMEZONE'))))->format('Y-m-d H:i:s');
     }
-	}
 
-	public static function init($config)
-	{
-		return new self($config);
-	}
-
-	protected function log(string $message)
-	{
-		$fileHandle = fopen($this->directory.'/'.'Migrations.History', 'a+');
-		fwrite($fileHandle, $message);
-		fclose($fileHandle);
-		echo "Schema Transaction Completed Successfully. \nView Transaction Log in Migration.History\n", "\n";
-	}
-
-	public function migrate()
-	{
-		try {
-			$dbname = getenv('DB_NAME');
-			$user = getenv('DB_USER');
-			$password = getenv('DB_PASS');
-			$server = getenv('DB_HOST');
-			$db = str_replace("pdo_", '', getenv('DB_DRIVER'));
-    	$conn = new PDO("$db:host=$server;dbname=$dbname", $user, $password);
-    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    	$this->parserIfExists($conn);
-    }catch(PDOException $e){
-	    echo $e->getMessage();
+    public function db($value)
+    {
+        try {
+            $user = env('DB_USER');
+            $password = env('DB_PASS');
+            $server = env('DB_HOST');
+            $db = str_replace("pdo_", '', env('DB_DRIVER'));
+            $collate = env("COLLATE", "utf8mb4_unicode_ci");
+            $conn = new PDO("$db:host=$server;", $user, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn->exec("CREATE DATABASE {$value} COLLATE {$collate};");
+            $this->log("DATABASE: \n\t[ NAME => $value, MODE => CREATE, CREATED_AT => {$this->time} ]\n");
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
-	}
 
-	public function populate()
-	{
-		try {
-			$dbname = getenv( 'DB_NAME');
-			$user = getenv( 'DB_USER');
-			$password = getenv( 'DB_PASS' );
-			$server = getenv( 'DB_HOST' );
-			$db = str_replace("pdo_", '', getenv('DB_DRIVER'));
-    	$conn = new PDO("$db:host=$server;dbname=$dbname", $user, $password);
-    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    	$this->insertIfExists($conn);
-    }catch(PDOException $e){
-	    echo $e->getMessage();
+    public static function init($config)
+    {
+        return new self($config);
     }
-	}
 
-	protected function queryMe($sql, $conn)
-	{
-		try {
-			$conn->exec($sql);
-    }catch(PDOException $e){
-	    echo $e->getMessage();
+    protected function log(string $message)
+    {
+        $fileHandle = fopen(ROOT.DS.'migration'.DS.'Migrations.log', 'a+');
+        fwrite($fileHandle, $message);
+        fclose($fileHandle);
+        echo "Schema Transaction Completed Successfully. \nView Transaction Log in Migration.History\n", "\n";
     }
-	}
 
-	protected function insertIfExists($scheme)
-	{
-		$data = @include $this->populator ?? throw new \Exception("Error Loading Population.php File", 1);
-		if (!empty($data)) {
-			foreach ($data as $table => $entry) {
-				Model::setTable($table)->insert($entry);
-				echo "Data has been inserted into ", $table, " table.";
-			}
-			$this->cleanFile($this->populator);
-		}
-	}
+    public function migrate()
+    {
+        try {
+            $dbname = env('DB_NAME');
+            $user = env('DB_USER');
+            $password = env('DB_PASS');
+            $server = env('DB_HOST');
+            $db = str_replace("pdo_", '', env('DB_DRIVER'));
+            $conn = new PDO("$db:host=$server;dbname=$dbname", $user, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->parserIfExists($conn);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
 
-	protected function parserIfExists($conn)
-	{
-		$migrator = @include $this->migrator ?? throw new \Exception("Error Loading Migration File", 1);
-		if ( !is_array($migrator) || empty($migrator)) {
-			echo "Migration.php must return Array & can not be empty";
-			return;
-		}
-		foreach ($migrator as $table => $columns) {
-			$queue = ["ALTER TABLE :table ADD PRIMARY KEY (id);", "ALTER TABLE :table  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;"];
-			$_sql = "CREATE TABLE `{$table}` ( id int(11) NOT NULL, ";
-			foreach($columns as $key => $value){
-				$_sql .= str_replace(":column", $key, $value[0]);
-				if($value[1] !== ''){
-					$queue[] = str_replace(":column", $key, str_replace(":table", $table, $value[1]) );
-				}
-			}
-			$this->queryMe(rtrim($_sql, ', ').");", $conn);
-			foreach ($queue as $key => $value) {
-				self::queryMe(str_replace(':table', $table, $value), $conn);
-			}
-			$columns = array_keys($columns);
-			$this->log(
-				"TABLE: \n\t[ NAME => $table, COLUMNS => $columns, MODE => MIGRATED, CREATED_AT => {$this->time} ]\n"
-			);
-		}
-		$this->cleanFile($this->migrator);
-	}
+    public function populate()
+    {
+        try {
+            $dbname = env('DB_NAME');
+            $user = env('DB_USER');
+            $password = env('DB_PASS');
+            $server = env('DB_HOST');
+            $db = str_replace("pdo_", '', env('DB_DRIVER'));
+            $conn = new PDO("$db:host=$server;dbname=$dbname", $user, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->insertIfExists($conn);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
 
-	public function cleanFile($file)
-	{
-		$fh = fopen($file, 'w');
-		fwrite($fh, "<?php return [];");
-		fclose($fh);
-	}
+    protected function queryMe($sql, $conn)
+    {
+        try {
+            $conn->exec($sql);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
 
-	public function integer($max_length=10)
-	{
-		return [":column int({$max_length}) NOT NULL, ", ""];
-	}
-	public function double($max_length=10)
-	{
-		return [ ":column double, ", ""];
-	}
-	public function float($max_length=10)
-	{
-		return [ ":column float({$max_length}), ", ""];
-	}
-	public function string($max_length, $null = false, $key='primary')
-	{
-		$null = ($null === false) ? "NOT NULL" : "NULL";
-		$t = "";
-		if(!empty($key)){
-			$key = strtolower($key);
-			if( $key === 'unique' ){
-				$t = "ALTER TABLE :table ADD UNIQUE :column (:column);";
-			}elseif( $key === 'fulltext' ){
-				$t = "ALTER TABLE :table ADD FULLTEXT KEY :column (:column);";
-			} elseif( $key === 'index' ){
-				$t = "CREATE INDEX :column ON :table (:column);";
-			}
-		}
-		if ($max_length > 63000) {
-			$type =  "text";
-		}elseif ($max_length <= 63000) {
-			$type = "varchar({$max_length})";
-		}elseif ($max_length < 18) {
-			$type =  "char({$max_length})" ;
-		}
-		return [ ":column {$type} {$null}, ", $t ];
-	}
-	public function oneOf(array $options, $default)
-	{
-		$options  = implode(', ', $options);
-		return [ ":column enum({$options}) NOT NULL DEFAULT {$default}, ", ""];
-	}
-	public function datetime()
-	{
-		return [ ":column DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, ", ""];
-	}
-	public function foreign_key($table, $column, $type=null, $length=10)
-	{
-		$type  = (strtolower($type) === 'string') ? "varchar({$length})" : 'int ';
-		return [":column {$type} NOT NULL,", "ALTER TABLE :table ADD FOREIGN KEY (:column) REFERENCES {$table}({$column});"];
-	}
+    protected function insertIfExists($scheme)
+    {
+        $data = @include $this->populator ?? exit("Error Loading Population.php File");
+        if (!empty($data)) {
+            foreach ($data as $table => $entry) {
+                Model::setTable($table)->insert($entry);
+                echo "Data has been inserted into ", $table, " table.";
+            }
+            $this->cleanFile($this->populator);
+        }
+    }
+
+    protected function parserIfExists($conn)
+    {
+        $migrator = @include $this->migrator ?? exit("Error Loading Migration File");
+        if (!is_array($migrator) || empty($migrator)) {
+            echo "Migration.php must return Array & can not be empty";
+            return;
+        }
+        foreach ($migrator as $table => $columns) {
+            $queue = ["ALTER TABLE :table ADD PRIMARY KEY (id);", "ALTER TABLE :table  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;"];
+            $_sql = "CREATE TABLE `{$table}` ( id int(11) NOT NULL, ";
+            foreach ($columns as $key => $value) {
+                $_sql .= str_replace(":column", $key, $value[0]);
+                if ($value[1] !== '') {
+                    $queue[] = str_replace(":column", $key, str_replace(":table", $table, $value[1]));
+                }
+            }
+            $this->queryMe(rtrim($_sql, ', ') . ");", $conn);
+            foreach ($queue as $key => $value) {
+                self::queryMe(str_replace(':table', $table, $value), $conn);
+            }
+            $columns = 'id, ' . implode(', ', array_keys($columns));
+            $this->log(
+                "TABLE: \n\t[ NAME => $table, COLUMNS => [ $columns ], MODE => MIGRATED, CREATED_AT => {$this->time} ]\n"
+            );
+            Console::model([$table]);
+        }
+        $this->cleanFile($this->migrator);
+    }
+
+    public function cleanFile($file)
+    {
+        $fh = fopen($file, 'w');
+        fwrite($fh, "<?php\nreturn [\n];");
+        fclose($fh);
+    }
+
+    public function integer($max_length = 10)
+    {
+        return [":column int({$max_length}) NOT NULL, ", ""];
+    }
+    public function double($max_length = 10)
+    {
+        return [ ":column double, ", ""];
+    }
+    public function float($max_length = 10)
+    {
+        return [ ":column float({$max_length}), ", ""];
+    }
+    public function string($max_length, $null = false, $key = 'primary')
+    {
+        $null = ($null === false) ? "NOT NULL" : "NULL";
+        $t = "";
+        if (!empty($key)) {
+            $key = strtolower($key);
+            if ($key === 'unique') {
+                $t = "ALTER TABLE :table ADD UNIQUE :column (:column);";
+            } elseif ($key === 'fulltext') {
+                $t = "ALTER TABLE :table ADD FULLTEXT KEY :column (:column);";
+            } elseif ($key === 'index') {
+                $t = "CREATE INDEX :column ON :table (:column);";
+            }
+        }
+        if ($max_length > 63000) {
+            $type =  "text";
+        } elseif ($max_length <= 63000) {
+            $type = "varchar({$max_length})";
+        } elseif ($max_length < 18) {
+            $type =  "char({$max_length})" ;
+        }
+        return [ ":column {$type} {$null}, ", $t ];
+    }
+    public function oneOf(array $options, $default)
+    {
+        $options  = implode(', ', $options);
+        return [ ":column enum({$options}) NOT NULL DEFAULT {$default}, ", ""];
+    }
+    public function datetime()
+    {
+        return [ ":column DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, ", ""];
+    }
+    public function foreign_key($table, $column, $type = null, $length = 10)
+    {
+        $type  = (strtolower($type) === 'string') ? "varchar({$length})" : 'int ';
+        return [":column {$type} NOT NULL,", "ALTER TABLE :table ADD FOREIGN KEY (:column) REFERENCES {$table}({$column});"];
+    }
 }
